a5022cafb7240f0809ed64dd9098a87d5f2b8723
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-10-27 17:02:38 +0600

    search icon customized

--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -202,7 +202,7 @@ const Navbar = () => {
                                     <span onClick={() => setShow(!show)} className='text-left hidden md:block  md:text-center  lg:mb-0 my-auto hover:text-red-600 cursor-pointer mr-[-50px]'><SearchIcon /></span>
                                     {
                                         show &&
-                                        <div className="">
+                                        <div className="hidden md:block ">
                                             <div className=' lg:flex  justify-center items-center absolute left-20 bottom-20 md:top-[130px] md:right-[-15vw] lg:top-[170px] lg:left-[50vw]'>
                                                 <input type="text" placeholder="Type here" class="input w-full max-w-xs rounded-full p-5 bg-slate-200 focus:outline-0 " />
                                                 <button className='w-10 h-10  rounded-full bg-primary text-white ml-[-45px] lg:ml-[-45px] hover:bg-black'><SearchIcon className='' /></button>
